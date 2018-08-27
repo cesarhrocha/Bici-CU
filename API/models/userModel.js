e74e5a9ee0fe86eb4e2b1ada7da090d34ff22759
César Hernández Rocha <cesarhrocha96@gmail.com>
2018-08-26 22:01:11 -0500

    RESTful API de usuarios lista

---
+++ API/models/userModel.js
@@ -0,0 +1,48 @@
+'use strict';
+var mongoose = require('mongoose');//Declaración de mongoose
+var Schema = mongoose.Schema;
+
+
+var UserSchema = new Schema({//Creación del esquema de usuarios
+  name: {
+    type: String,
+    required: 'Enter the users name'
+  },
+  secondName: {
+      type: String,
+      required: 'Enter the users second name'
+  },
+  fullName: {
+    type: String,
+    required: 'Enter the users full name'
+  },
+  institutionalId: {
+    type: String,
+    required: 'Enter the users institutional identification number'
+  },
+  email: {
+    type: String,
+    required: 'Enter the users emailemail'
+  },
+  password: {
+    type: String,
+    required: 'Enter the users password'
+  },
+  picture: {
+    type: String,
+    default: 'default.jpg'
+  },
+  userType: {
+    type: [{
+      type: String,
+      enum: ['Student', 'Vigilant'],
+      default: ['Student']
+    }]
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Users', UserSchema);
